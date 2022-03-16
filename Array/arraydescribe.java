315a016c817493e75fddf9c45ca8809d31a6bb9a
Kaustubh Ratna <kaustubhratna@gmail.com>
2022-03-16 16:01:49 +0530

    array first commit

---
+++ Array/arraydescribe.java
@@ -0,0 +1,10 @@
+package Array;
+
+public class arraydescribe {
+    public static void main(String[] args) {
+        
+    
+    System.out.println("this folder contains programs using array");
+}
+
+}
